8bf0f9ae76ad885fda8e9857220c50c6143d11d8
github username <github email address>
2019-12-18 15:30:27 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  #split the string on any and all periods, question marks and exclamation marks.
   def count_sentences
+    # binding.pry
 
+=begin 
+  /\.|\?|!/ this syntax for .split was found @https://stackoverflow.com/questions/32451429/count-number-of-sentence-ruby
+  self.split(/\.|\?|!/) splits string at . ? ! and returns an array
+  but ^^ also returns an array with empty ("") elements
+  .reject{|x| x.empty?} returns an array with empty elements removed
+  .count returns # of strings
+=end 
+
+    #solution
+    count_sentence = self.split(/\.|\?|!/).reject{|x| x.empty?}.count
+    count_sentence
+    
   end
 end
